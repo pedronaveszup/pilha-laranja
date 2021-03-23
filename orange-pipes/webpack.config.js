3cc4d5423fcab8240419066f2a0a4c71db8b943e
pedronaveszup <pedro.naves@zup.com.br>
2021-03-23 13:04:24 -0300

    orange-pipes in angular

---
+++ orange-pipes/webpack.config.js
@@ -0,0 +1,45 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, 'tsconfig.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    publicPath: "http://localhost:4200/",
+    uniqueName: "orange-pipes"
+  },
+  optimization: {
+    runtimeChunk: false
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      
+        // For remotes (please adjust)
+        name: "pipes",
+        filename: "remoteEntry.js",
+        exposes: {
+          './Component': './src/main.ts',
+        },        
+        
+        // For hosts (please adjust)
+        // remotes: {
+        //     "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
+
+        // },
+
+        shared: {
+          "@angular/core": { singleton: true, strictVersion: true }, 
+          "@angular/common": { singleton: true, strictVersion: true }, 
+          "@angular/router": { singleton: true, strictVersion: true },
+
+          ...sharedMappings.getDescriptors()
+        }
+        
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
